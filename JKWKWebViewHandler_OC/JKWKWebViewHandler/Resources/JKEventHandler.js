be1a84a86c91ffc958b033a666890b48e7c9c06a
wangxian <xian366@126.com>
2021-10-19 13:19:27 +0800

    修改：删除调试日志

--- JKWKWebViewHandler_OC/JKWKWebViewHandler/Resources/JKEventHandler.js
+++ JKWKWebViewHandler_OC/JKWKWebViewHandler/Resources/JKEventHandler.js
@@ -1,90 +1,92 @@
 var JKEventHandler = {
-    exec: function (plugin, funcName, params, successCallBack, failureCallBack) {
-        var message;
-        var timeStamp = new Date().getTime();
-        var successCallBackID = plugin + '_' + funcName + '_' + timeStamp + '_' + 'successCallBack';
-        var failureCallBackID = plugin + '_' + funcName + '_' + timeStamp + '_' + 'failureCallBack';
+  exec: function (plugin, funcName, params, successCallBack, failureCallBack) {
+      var message;
+      var timeStamp = new Date().getTime();
+      var successCallBackID = plugin + '_' + funcName + '_' + timeStamp + '_' + 'successCallBack';
+      var failureCallBackID = plugin + '_' + funcName + '_' + timeStamp + '_' + 'failureCallBack';
 
-        if (successCallBack) {
-            if (!JKBridgeEvent._listeners[successCallBackID]) {
-                JKBridgeEvent.addEvent(successCallBackID, function (data) {
-                    successCallBack(data);
-                });
-            }
-        }
+      if (successCallBack) {
+          if (!JKBridgeEvent._listeners[successCallBackID]) {
+              JKBridgeEvent.addEvent(successCallBackID, function (data) {
+                  successCallBack(data);
+              });
+          }
+      }
 
-        if (failureCallBack) {
-            if (!JKBridgeEvent._listeners[failureCallBackID]) {
-                JKBridgeEvent.addEvent(failureCallBackID, function (data) {
-                    failureCallBack(data);
-                });
-            }
-        }
+      if (failureCallBack) {
+          if (!JKBridgeEvent._listeners[failureCallBackID]) {
+              JKBridgeEvent.addEvent(failureCallBackID, function (data) {
+                  failureCallBack(data);
+              });
+          }
+      }
 
-        if (successCallBack && failureCallBack) {
-            message = {
-                'plugin': plugin,
-                'func': funcName,
-                'params': params,
-                'successCallBackID': successCallBackID,
-                'failureCallBackID': failureCallBackID
-            };
-        } else if (successCallBack && !failureCallBack) {
-            message = {
-                'plugin': plugin,
-                'func': funcName,
-                'params': params,
-                'successCallBackID': successCallBackID
-            };
-        } else if (failureCallBack && !successCallBack) {
-            message = {
-                'plugin': plugin,
-                'func': funcName,
-                'params': params,
-                'failureCallBackID': failureCallBackID
-            };
-        } else {
-            message = {
-                'plugin': plugin,
-                'func': funcName,
-                'params': params
-            };
-        }
+      if (successCallBack && failureCallBack) {
+          message = {
+              'plugin': plugin,
+              'func': funcName,
+              'params': params,
+              'successCallBackID': successCallBackID,
+              'failureCallBackID': failureCallBackID
+          };
+      } else if (successCallBack && !failureCallBack) {
+          message = {
+              'plugin': plugin,
+              'func': funcName,
+              'params': params,
+              'successCallBackID': successCallBackID
+          };
+      } else if (failureCallBack && !successCallBack) {
+          message = {
+              'plugin': plugin,
+              'func': funcName,
+              'params': params,
+              'failureCallBackID': failureCallBackID
+          };
+      } else {
+          message = {
+              'plugin': plugin,
+              'func': funcName,
+              'params': params
+          };
+      }
 
-        window.webkit.messageHandlers.JKEventHandler.postMessage(message);
-    },
+      window.webkit.messageHandlers.JKEventHandler.postMessage(message);
+  },
 
-    callBack: function (callBackID, data) {
-        JKBridgeEvent.fireEvent(callBackID, data);
-        JKBridgeEvent.removeEvent(callBackID);
-    },
+  callBack: function (callBackID, data) {
+      JKBridgeEvent.fireEvent(callBackID, data);
+      JKBridgeEvent.removeEvent(callBackID);
+  },
 
-    removeAllCallBacks: function (data) {
-        JKBridgeEvent._listeners = {};
-    }
+  removeAllCallBacks: function (data) {
+      JKBridgeEvent._listeners = {};
+  }
 };
 
 var JKBridgeEvent = {
-    _listeners: {},
-    addEvent: function (callBackID, fn) {
-        this._listeners[callBackID] = fn;
-        return this;
-    },
+  _listeners: {},
+  addEvent: function (callBackID, fn) {
+      this._listeners[callBackID] = fn;
+      return this;
+  },
 
-    fireEvent: function (callBackID, param) {
-        var fn = this._listeners[callBackID];
+  fireEvent: function (callBackID, param) {
+      var fn = this._listeners[callBackID];
 
-        if (typeof callBackID === "string" && typeof fn === "function") {
-            fn(param);
-        } else {
-            delete this._listeners[callBackID];
-        }
+      if (typeof callBackID === "string" && typeof fn === "function") {
+          fn(param);
+      } else {
+          delete this._listeners[callBackID];
+      }
 
-        return this;
-    },
+      return this;
+  },
 
-    removeEvent: function (callBackID) {
-        delete this._listeners[callBackID];
-        return this;
-    }
+  removeEvent: function (callBackID) {
+      let callBackIDPrefix = callBackID.substr(0, callBackID.length - 15);
+      delete this._listeners[callBackIDPrefix + "successCallBack"];
+      delete this._listeners[callBackIDPrefix + "failureCallBack"];
+      return this;
+  }
 }
